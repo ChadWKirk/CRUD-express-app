575974c3236a907bf698705db08e550f6f4cd2e0
Chad Kirk <ChadWKirk@gmail.com>
2022-08-10 15:17:08 -0500

    made ejs. 11:30 into web dev simplified video

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const PORT = process.env.PORT || 5000;
 
 const Members = [{ name: "charlie" }];
 
-app.use(express.json()); //parses incoming requests with JSON payloads (basically turns req data into JSON).
-app.use(express.urlencoded({ extended: false })); //Turns body into (parses) urlencoded data then parses into querystring library & creates a req.body object
+//app.use(express.json()); //parses incoming requests with JSON payloads (basically turns req data into JSON).
+//app.use(express.urlencoded({ extended: false })); //Turns body into (parses) urlencoded data then parses into querystring library & creates a req.body object
+
+app.set("view engine", "ejs");
 
 //Homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.render("index", { name: "world" });
 });
+//app.get("/", (req, res) => {
+//res.sendFile(path.join(__dirname, "public", "index.html"));
+//});
 
 //Members page
 app.get("/members", (req, res) => {
@@ -19,7 +24,6 @@ app.get("/members", (req, res) => {
 });
 
 app.post("/members", (req, res) => {
-  
   const newMember = { name: req.body.name, email: req.body.email };
 
   Members.push(newMember);

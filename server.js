7788f2e72eaf2f19d8853e1d567abab8c56de269
Chad Kirk <ChadWKirk@gmail.com>
2022-08-10 16:23:50 -0500

    working on ejs

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-const Members = [{ name: "charlie" }];
+const Members = [
+  { name: "charlie", email: "charlie@gmail.com" },
+  { name: "david", email: "david@gmail.com" },
+];
 
 //app.use(express.json()); //parses incoming requests with JSON payloads (basically turns req data into JSON).
 //app.use(express.urlencoded({ extended: false })); //Turns body into (parses) urlencoded data then parses into querystring library & creates a req.body object
@@ -12,7 +15,7 @@ app.set("view engine", "ejs");
 
 //Homepage
 app.get("/", (req, res) => {
-  res.render("index", { name: "world" });
+  res.render("index", res.json({ Members }));
 });
 //app.get("/", (req, res) => {
 //res.sendFile(path.join(__dirname, "public", "index.html"));

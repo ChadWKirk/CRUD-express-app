f2ebe266b596686834435f641aadbba98514d7ea
Chad Kirk <santaomgprod@gmail.com>
2022-08-11 19:49:04 -0500

    made post request button work

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
 
 const PORT = process.env.PORT || 5000;
 
+const Members = [
+  { name: "Charlie", email: "Charlie@gmail.com" },
+  { name: "William", email: "William@gmail.com" },
+];
+
+//Middleware to parse body into json
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.set("view engine", "ejs");
 
+//Retrieve
 app.get("/", (req, res) => {
-  res.render("index");
-  console.log("test");
+  res.render("index", { Members: Members });
 });
+
+//Create
+app.post("/members", (req, res) => {
+  const newMember = { name: req.body.name, email: req.body.email };
+  Members.push(newMember);
+  res.redirect("/");
+});
+
+//Delete
+app.delete("/members", (req, res) => {
+  console.log("testing");
+});
+
+//Update
 
 app.listen(PORT);
